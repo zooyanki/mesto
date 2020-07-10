bc8226bffba756c1c68d530b661a879ee383bfdd
zooyanki <zooyanki@yandex.ru>
2020-07-10 15:30:30 +0300

    3

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
+let container = document.querySelector('.page');
 let EditButton = document.querySelector('.profile__edit-button');
 let CloseButton = document.querySelector('.editor__button-close');
 let SubmitButton = document.querySelector('.editor__submit-button');
-let container = document.querySelector('.page');
 let ProfileStatus = container.querySelector('.profile__status-contain');
 let ProfileName = container.querySelector('.profile__name-contain');
-
 let EditorForm = document.querySelector('.editor');
 let EditorChangeStatus = container.querySelector('.editor__field-status');
 let EditorChangeName = container.querySelector('.editor__field-name');
+let Overlay = document.querySelector('.overlay');
 
 
 EditButton.onclick = function() {
     EditorForm.style.display = "block";
+    Overlay.style.display = "block";
 }
 
 CloseButton.onclick = function() {
     EditorForm.style.display = "none";
+    Overlay.style.display = "none";
 }
 
 function EditStatus() {
@@ -28,6 +30,7 @@ EditStatus();
 SubmitButton.addEventListener('click', EditStatus);
 SubmitButton.onclick = function() {
     EditorForm.style.display = "none";
+    Overlay.style.display = "none";
 }
 
 function EditName() {
@@ -40,4 +43,5 @@ EditName();
 SubmitButton.addEventListener('click', EditName);
 SubmitButton.onclick = function() {
     EditorForm.style.display = "none";
+    Overlay.style.display = "none";
 }
