cf363a3f27e57d67b15d11c364bc9745f4d9eacb
zooyanki <zooyanki@yandex.ru>
2020-07-21 20:50:18 +0300

    40

--- script/script.js
+++ script/script.js
@@ -8,6 +8,8 @@ let profileStatus = document.querySelector('.profile__status');
 let nameInput = document.querySelector('.editor__field_name');
 let overlay = document.querySelector('.overlay');
 let formEditor = document.querySelector('.editor__form');
+let addButton = document.querySelector('.profile__add-button');
+let elements = document.querySelector('.elements');
 
 // Открытие редактора профиля0
 function editorOpenButton() {
@@ -36,3 +38,47 @@ function formSubmitHandler (evt) {
 editButton.addEventListener('click', editorOpenButton);
 closeButton.addEventListener('click', editorCloseButton);
 formEditor.addEventListener('submit', formSubmitHandler);
+
+//Блок Элемент - добавление карточек
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+function addPicture() {   
+
+    const template = document.querySelector('.template').content;
+    
+    
+    for (let i = 0; i < initialCards.length; i++) {
+    const userImage = template.cloneNode(true);
+    userImage.querySelector('.element__image').src = initialCards[i].link;
+    userImage.querySelector('.element__text').textContent = initialCards[i].name;
+
+    elements.append(userImage);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', addPicture);
