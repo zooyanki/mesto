27eae23b40bc1d84c55a6b190bc368b95a8889e1
zooyanki <zooyanki@yandex.ru>
2020-07-29 23:37:40 +0300

    70

--- script/script.js
+++ script/script.js
@@ -1,59 +1,68 @@
-let editButton = document.querySelector('.profile__edit-button');
-let profileName = document.querySelector('.profile__name');
-let profileStatus = document.querySelector('.profile__status');
-let addButton = document.querySelector('.profile__add-button');
+const editButton = document.querySelector('.profile__edit-button');
+const profileName = document.querySelector('.profile__name');
+const profileStatus = document.querySelector('.profile__status');
+const addButton = document.querySelector('.profile__add-button');
 //Переменные "Модального окна - общие"
-let closeButtons = document.querySelectorAll('.modal__button-close'); //Кнопка закрытия окна
+const closeButtons = document.querySelectorAll('.modal__button-close'); //Кнопка закрытия окна
 //Переменные "Модального окна - Профиль"
-let editor = document.querySelector('.modal_editor');
-let submitButton = document.querySelector('.modal__submit-button');
-let statusInput = document.querySelector('.modal__editor-field_status');
-let nameInput = document.querySelector('.modal__editor-field_name');
-let formEditor = document.querySelector('.modal__form_editor');
+const editor = document.querySelector('.modal_editor');
+const submitButton = document.querySelector('.modal__submit-button');
+const statusInput = document.querySelector('.modal__editor-field_status');
+const nameInput = document.querySelector('.modal__editor-field_name');
+const formEditor = document.querySelector('.modal__form_editor');
 //Переменные "Модального окна - добавление рендера"
-let formNewform = document.querySelector('.modal__form_newform');
-let newForm = document.querySelector('.modal_newform');
-let inputNewformName = document.querySelector('.modal__field_newform-name');
-let inputNewformLink = document.querySelector('.modal__field_newform-link');
+const formNewform = document.querySelector('.modal__form_newform');
+const newForm = document.querySelector('.modal_newform');
+const inputNewformName = document.querySelector('.modal__field_newform-name');
+const inputNewformLink = document.querySelector('.modal__field_newform-link');
 const template = document.querySelector('.template').content;
 //Переменные "Модального окна - увеличенный рендер"
-let render = document.querySelector('.modal_render');
-let renderImage = document.querySelector('.modal__render');
-let renderText = document.querySelector('.modal__render-text');
+const render = document.querySelector('.modal_render');
+const renderImage = document.querySelector('.modal__render');
+const renderText = document.querySelector('.modal__render-text');
 //Переменные "Карточки с рендером"
-let elements = document.querySelector('.elements');
+const elements = document.querySelector('.elements');
 
+//Функция открытия окна
+function modalOpen(popup){
+    popup.classList.add('modal_opened');
+}
 
 // Открытие редактора профиля
 function editorOpenButton() {
-    editor.classList.add('modal_open');
+    modalOpen(editor);
     nameInput.value = profileName.textContent;
     statusInput.value = profileStatus.textContent;
 }
 
 //Открытие окна добавления рендера
 function newFormOpenButton() {
-    newForm.classList.add('modal_open');
+    inputNewformName.value = '';
+    inputNewformLink.value = '';
+    modalOpen(newForm);
 }
 
-// Закрытие модального окна
-function modalCloseButton() {
-    newForm.classList.remove('modal_open');
-    editor.classList.remove('modal_open');
-    render.classList.remove('modal_open');
+
+// Закрытие модальных окон
+function modalClose(popup){
+    popup.classList.remove('modal_opened');
 }
 
-for (let i=0; i<closeButtons.length; i++) {
-closeButtons[i].addEventListener('click', modalCloseButton);
-}
+const closeBtnNewForm = newForm.querySelector(".modal__button-close");
+const closeBtnEditor = editor.querySelector(".modal__button-close");
+const closeBtnRender = render.querySelector(".modal__button-close");
+
+closeBtnNewForm.addEventListener('click', () => modalClose(newForm));
+closeBtnEditor.addEventListener('click', () => modalClose(editor));
+closeBtnRender.addEventListener('click', () => modalClose(render));
 
 
 // Обработчик «отправки» формы
-function formSubmitHandler (evt) {
+function formProfileSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileStatus.textContent = statusInput.value;
-    modalCloseButton();
+    modalClose(editor);
 }
 
 //Автоматическое добавление карточек на главную страницу
@@ -84,37 +93,49 @@ const initialCards = [
     }
 ];
 
+let cards = initialCards;
+
 function basicRender() { 
     elements.innerHTML = '';       
-    for (let i = 0; i < initialCards.length; i++) {
-        const card = template.cloneNode(true);
-        card.querySelector('.element__image').src = initialCards[i].link;
-        card.querySelector('.element__text').textContent = initialCards[i].name;
+
+    cards.forEach((item, i) => {
+        const card = createCard(item.name, item.link, i);
+        elements.append(card);
+    })
+}
+basicRender();
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function createCard(name, link, index) {
+    const card = template.cloneNode(true);
+    const elementImage = card.querySelector('.element__image');
+    elementImage.src = link;
+    card.querySelector('.element__text').textContent = name;
+    
     //Лайк - "да" или "нет"
     card.querySelector('.element__group').addEventListener('click', function (evt){ 
         evt.target.classList.toggle('element__group_black');
-    });
-
+    });   
+    
     // Удаление картинок
-    let trash = card.querySelector('.element__trash');
+    const trash = card.querySelector('.element__trash');
 
-    trash.addEventListener('click', function() { 
-        initialCards.splice(i, 1);
+    trash.addEventListener('click', function() {
+        cards = cards.filter((e, ind) => index != ind);
         basicRender();
     });
 
     //Открытие окна увеличенной картинки
-    const elementImage = card.querySelector('.element__image');
-    const elementText = card.querySelector('.element__text');
     elementImage.addEventListener('click', function(){ 
-        render.classList.add('modal_open');   
-        renderImage.src = elementImage.src;
-        renderText.textContent = elementText.textContent;
+        modalOpen(render);   
+        renderImage.src = link;
+        renderText.textContent = name;
     });
-    elements.append(card);
-    }
+
+    return card;
 }
-document.addEventListener('DOMContentLoaded', basicRender);
+
+
+
 
 
 // Обработчик - добавление картинок
@@ -124,15 +145,16 @@ function addRender (ren) {
         name: inputNewformName.value,
         link: inputNewformLink.value
     }
-    initialCards.unshift(newCard);
+    cards.unshift(newCard);
     basicRender();
-    modalCloseButton();
+    modalClose(newForm);
 }
 
 editButton.addEventListener('click', editorOpenButton);
 addButton.addEventListener('click', newFormOpenButton);
-formEditor.addEventListener('submit', formSubmitHandler);
+formEditor.addEventListener('submit', formProfileSubmitHandler);
 newForm.addEventListener('submit', addRender);
 
 
     
+
